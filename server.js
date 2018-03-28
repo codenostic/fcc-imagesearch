4aaf00cc95f2ca3ab6ae3ae59d810f03068b70f6
Glitch (fcc-imagesearch-api) <none>
2018-03-28 10:13:26 +0000

    added routes for search and history

--- server.js
+++ server.js
@@ -5,6 +5,14 @@
 const express = require('express')
 const app = express()
 var searchController = require('./controllers/searchController')
+var mongoose = require('mongoose');
+
+//connecting to database 
+var mongoDB = "mongodb://root:root123@ds143734.mlab.com:43734/search_lib";
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('errors', console.error.bind(console, 'connection error:'))
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
